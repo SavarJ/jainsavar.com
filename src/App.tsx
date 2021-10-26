83f136489abb3185530232f2029845eff9d051ef
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2021-10-25 22:06:25 -0400

    Added tailwind css successfully

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-red-500 flex font-bold">Hello world</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
