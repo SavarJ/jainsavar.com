f81bec3f5e2ad5bbee41f5014355fd3531ff83c4
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2021-10-28 20:11:16 -0400

    Fix mobile responsiveness for place holder website and minor change. Pushed to prod

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,11 +10,12 @@ const App: React.FC = () => {
   return (
     <main className="max-w-full">
       {/* Under construction page */}
-      <div className="min-h-screen flex items-center justify-center flex-col">
-        <h1 className="font-bold text-5xl p-4 text-blue-700">Savar Jain</h1>
+      <div className="min-h-screen flex items-center justify-center flex-col p-5 text-center">
+        <h1 className="font-bold text-5xl p-3 text-blue-700">Savar Jain</h1>
         <h1>
-          I am still in the process of building this personal portfolio site
+          I am still in the process of building this personal portfolio site.
         </h1>
+        <hr className="border-2 h-1 w-full my-3" />
         <h2>In the mean time, check out:</h2>
         <div className="flex w-48 justify-evenly text-blue-800 pt-2">
           <a href="https://github.com/SavarJ">
