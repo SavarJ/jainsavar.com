bad6bfcb4d0c055ea3be8ac1b7f96560741d3ed1
Savar Jain: School Laptop <jainsavar@gmail.com>
2022-01-05 20:42:21 -0500

    Refactoring code

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,20 +1,41 @@
 import "./custom.css";
 import Link from "./Link";
+import { NormalSection, SectionBody, SectionTitle } from "./Components";
 const ProjectsSection: React.FC = () => {
   return (
-    <section>
-      <h1 className="text-5xl text-blue-200 font-bold mt-4" style={{ color: "#19B5FE" }}>
-        {"<Projects />"}
-      </h1>
-      <div className="max-w-3xl text-gray-400 mt-4 flex flex-wrap justify-between -ml-2">
-        {projectsList.map((project, index) => (
-          <Project key={index} project={project} />
-        ))}
-      </div>
-    </section>
+    <NormalSection>
+      <SectionTitle class="-mb-4">{"<Projects />"}</SectionTitle>
+      <SectionBody class="flex flex-wrap justify-between -ml-2">
+        <Projects />
+      </SectionBody>
+    </NormalSection>
   );
 };
 
+const Projects: React.FC = () => {
+  return (
+    <>
+      {projectsList.map((project, index) => (
+        <Project key={index} project={project} />
+      ))}
+    </>
+  );
+};
+
+const Project: React.FC<ProjectProps> = ({ project }) => {
+  return (
+    <ProjectCard>
+      <ProjectCardHead>
+        <ProjectCardHeadTitle>{project.title}</ProjectCardHeadTitle>
+        <ProjectCardHeadDescription>{project.description}</ProjectCardHeadDescription>
+      </ProjectCardHead>
+      <ProjectCardBody>
+        <ProjectCardBodyLanguages project={project} />
+        <ProjectCardBodyLinks project={project} />
+      </ProjectCardBody>
+    </ProjectCard>
+  );
+};
 interface projectInterface {
   title: string;
   description: string;
@@ -26,37 +47,61 @@ interface ProjectProps {
   project: projectInterface;
 }
 
-const Project: React.FC<ProjectProps> = ({ project }) => {
+interface ComponentProps {
+  children: React.ReactNode;
+  class?: string;
+}
+
+const ProjectCard: React.FC<ComponentProps> = (props) => {
   return (
     <div className="rounded-lg p-6 shadow-xl w-88 h-68 m-4 flex flex-col justify-between hover:shadow-4xl relative project">
-      <div>
-        <h3 className="font-bold text-2xl text-blue-50">{project.title}</h3>
-        <p className="text-gray-200 py-2 text-base">{project.description}</p>
-      </div>
-      <div>
-        <p className="text-gray-400 italic text-sm">
-          {project.languages.map((language, index) => {
-            return (
-              <span
-                key={index}
-                className="inline-block bg-gray-800 text-white rounded-full px-3 py-1 mr-2 mb-2"
-              >
-                {language}
-              </span>
-            );
-          })}
-        </p>
-        <div className="flex justify-evenly mt-2">
-          <Link link={project.githubLink}>
-            <i className="fa fa-github text-xl"></i>
-          </Link>
-          {project.liveLink && (
-            <Link link={project.liveLink}>
-              <i className="fas fa-external-link-alt text-xl "></i>
-            </Link>
-          )}
-        </div>
-      </div>
+      {props.children}
+    </div>
+  );
+};
+
+const ProjectCardHead: React.FC<ComponentProps> = (props) => {
+  return <div>{props.children}</div>;
+};
+const ProjectCardHeadTitle: React.FC<ComponentProps> = (props) => {
+  return <h3 className="font-bold text-2xl text-blue-50">{props.children}</h3>;
+};
+const ProjectCardHeadDescription: React.FC<ComponentProps> = (props) => {
+  return <p className="text-gray-200 py-2 text-base">{props.children}</p>;
+};
+
+const ProjectCardBody: React.FC<ComponentProps> = (props) => {
+  return <div>{props.children}</div>;
+};
+
+interface ProjectCardBodyProps {
+  project: projectInterface;
+}
+const ProjectCardBodyLanguages: React.FC<ProjectCardBodyProps> = ({ project }) => {
+  return (
+    <p className="text-gray-400 italic text-sm">
+      {project.languages.map((language, index) => {
+        return (
+          <span key={index} className="inline-block bg-gray-800 text-white rounded-full px-3 py-1 mr-2 mb-2">
+            {language}
+          </span>
+        );
+      })}
+    </p>
+  );
+};
+
+const ProjectCardBodyLinks: React.FC<ProjectCardBodyProps> = ({ project }) => {
+  return (
+    <div className="flex justify-evenly mt-2">
+      <Link link={project.githubLink}>
+        <i className="fa fa-github text-xl"></i>
+      </Link>
+      {project.liveLink && (
+        <Link link={project.liveLink}>
+          <i className="fas fa-external-link-alt text-xl "></i>
+        </Link>
+      )}
     </div>
   );
 };
