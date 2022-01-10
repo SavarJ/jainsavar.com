8d5bd283a44885f11eb3e97607f93f720cf3c01b
Savar Jain: School Laptop <jainsavar@gmail.com>
2022-01-10 09:11:09 -0500

    Adding typing animation to whole home page. Still not completely done

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -2,16 +2,83 @@ import Typist from "react-typist";
 import { FullSection } from "./Components";
 import Link from "./Link";
 import "./custom.css";
+import { useState } from "react";
 const HomeSection: React.FC = () => {
+  const [selfHeadingDone, setSelfHeadingDone] = useState(false);
+  const [selfDescriptionDone, setSelfDescriptionDone] = useState(false);
   return (
     <FullSection class="-mb-40">
       <SelfLogo />
-      <SelfHeading />
-      <SelfDescription />
+      <SelfHeading setSelfHeadingDone={setSelfHeadingDone} />
+      <SelfDescription selfHeadingDone={selfHeadingDone} setSelfDescriptionDone={setSelfDescriptionDone} />
       <br />
-      <SelfSubDescription />
+      <SelfSubDescription selfDescriptionDone={selfDescriptionDone} />
       <Socials />
     </FullSection>
+  );
+};
+
+const SelfLogo: React.FC = () => {
+  return <img src="/logo.png" alt="Logo-img" className="h-40 mb-8" />;
+};
+interface SelfHeadingProps {
+  setSelfHeadingDone: (value: boolean) => void;
+}
+
+const SelfHeading: React.FC<SelfHeadingProps> = ({ setSelfHeadingDone }) => {
+  return (
+    <Typist
+      startDelay={1100}
+      avgTypingDelay={120}
+      cursor={{ hideWhenDone: true }}
+      className="text-7xl font-inconsolata inline-block"
+      onTypingDone={() => setSelfHeadingDone(true)}
+    >
+      Hi, I'm{" "}
+      <span className="font-bold tracking-wide" style={{ color: "#19B5FE" }}>
+        Savar
+      </span>
+      !
+    </Typist>
+  );
+};
+interface SelfDescriptionProps {
+  selfHeadingDone: boolean;
+  setSelfDescriptionDone: (value: boolean) => void;
+}
+const SelfDescription: React.FC<SelfDescriptionProps> = ({ selfHeadingDone, setSelfDescriptionDone }) => {
+  return (
+    <>
+      {selfHeadingDone && (
+        <Typist
+          className="mt-4 text-2xl max-w-3xl text-gray-400 font-zb"
+          onTypingDone={() => setSelfDescriptionDone(true)}
+        >
+          I'm a high school senior who's passionate about Computer Science and Software Engineering.
+        </Typist>
+      )}
+    </>
+  );
+};
+interface SelfSubDescriptionProps {
+  selfDescriptionDone: boolean;
+}
+const SelfSubDescription: React.FC<SelfSubDescriptionProps> = ({ selfDescriptionDone }) => {
+  return (
+    <>
+      {selfDescriptionDone && (
+        <Typist className="text-xl text-gray-400 font-zb">
+          Feel free to check out my{" "}
+          <Link link="/assets/SavarJain-Resume" color="orange">
+            resume
+          </Link>{" "}
+          or email me at{" "}
+          <Link link="mailto:jainsavar@gmail.com" color="orange">
+            jainsavar@gmail.com
+          </Link>
+        </Typist>
+      )}
+    </>
   );
 };
 
@@ -34,44 +101,4 @@ const Socials: React.FC = () => {
     </div>
   );
 };
-
-const SelfLogo: React.FC = () => {
-  return <img src="/logo.png" alt="Logo-img" className="h-40 mb-8" />;
-};
-const SelfHeading: React.FC = () => {
-  return (
-    // show the cursor
-    <Typist startDelay={300} avgTypingDelay={120}>
-      <h1 className="text-7xl font-inconsolata inline-block">
-        Hi, I'm{" "}
-        <span className="font-bold tracking-wide" style={{ color: "#19B5FE" }}>
-          Savar
-        </span>
-        !
-      </h1>
-    </Typist>
-  );
-};
-const SelfDescription: React.FC = () => {
-  return (
-    <h2 className="mt-4 text-2xl max-w-3xl text-gray-400 font-zb">
-      I'm a high school senior who's passionate about Computer Science and Software Engineering.
-    </h2>
-  );
-};
-const SelfSubDescription: React.FC = () => {
-  return (
-    <h3 className="text-xl text-gray-400 font-zb">
-      Feel free to check out my{" "}
-      <Link link="/assets/SavarJain-Resume" color="orange">
-        resume
-      </Link>{" "}
-      or email me at{" "}
-      <Link link="mailto:jainsavar@gmail.com" color="orange">
-        jainsavar@gmail.com
-      </Link>
-    </h3>
-  );
-};
-
 export default HomeSection;
