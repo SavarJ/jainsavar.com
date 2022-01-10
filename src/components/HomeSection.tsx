45e126b3deee90edd2a1b259f3015c45144e38df
Savar Jain: School Laptop <jainsavar@gmail.com>
2022-01-10 10:37:08 -0500

    Finished typing animation of home page

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -3,17 +3,23 @@ import { FullSection } from "./Components";
 import Link from "./Link";
 import "./custom.css";
 import { useState } from "react";
+const Zoom = require("react-reveal/Zoom");
+
 const HomeSection: React.FC = () => {
   const [selfHeadingDone, setSelfHeadingDone] = useState(false);
   const [selfDescriptionDone, setSelfDescriptionDone] = useState(false);
+  const [selfSubDescriptionDone, setSelfSubDescriptionDone] = useState(false);
   return (
     <FullSection class="-mb-40">
       <SelfLogo />
       <SelfHeading setSelfHeadingDone={setSelfHeadingDone} />
       <SelfDescription selfHeadingDone={selfHeadingDone} setSelfDescriptionDone={setSelfDescriptionDone} />
       <br />
-      <SelfSubDescription selfDescriptionDone={selfDescriptionDone} />
-      <Socials />
+      <SelfSubDescription
+        selfDescriptionDone={selfDescriptionDone}
+        setSelfSubDescription={setSelfSubDescriptionDone}
+      />
+      <Socials selfSubDescriptionDone={selfSubDescriptionDone} />
     </FullSection>
   );
 };
@@ -25,14 +31,14 @@ interface SelfHeadingProps {
   setSelfHeadingDone: (value: boolean) => void;
 }
 
-const SelfHeading: React.FC<SelfHeadingProps> = ({ setSelfHeadingDone }) => {
+const SelfHeading: React.FC<SelfHeadingProps> = (props) => {
   return (
     <Typist
+      className="text-7xl font-inconsolata inline-block"
       startDelay={1100}
       avgTypingDelay={120}
-      cursor={{ hideWhenDone: true }}
-      className="text-7xl font-inconsolata inline-block"
-      onTypingDone={() => setSelfHeadingDone(true)}
+      cursor={{ hideWhenDone: true, hideWhenDoneDelay: 100 }}
+      onTypingDone={() => props.setSelfHeadingDone(true)}
     >
       Hi, I'm{" "}
       <span className="font-bold tracking-wide" style={{ color: "#19B5FE" }}>
@@ -46,13 +52,15 @@ interface SelfDescriptionProps {
   selfHeadingDone: boolean;
   setSelfDescriptionDone: (value: boolean) => void;
 }
-const SelfDescription: React.FC<SelfDescriptionProps> = ({ selfHeadingDone, setSelfDescriptionDone }) => {
+const SelfDescription: React.FC<SelfDescriptionProps> = (props) => {
   return (
     <>
-      {selfHeadingDone && (
+      {props.selfHeadingDone && (
         <Typist
           className="mt-4 text-2xl max-w-3xl text-gray-400 font-zb"
-          onTypingDone={() => setSelfDescriptionDone(true)}
+          avgTypingDelay={50}
+          cursor={{ hideWhenDone: true, hideWhenDoneDelay: 100 }}
+          onTypingDone={() => props.setSelfDescriptionDone(true)}
         >
           I'm a high school senior who's passionate about Computer Science and Software Engineering.
         </Typist>
@@ -62,12 +70,16 @@ const SelfDescription: React.FC<SelfDescriptionProps> = ({ selfHeadingDone, setS
 };
 interface SelfSubDescriptionProps {
   selfDescriptionDone: boolean;
+  setSelfSubDescription: (value: boolean) => void;
 }
-const SelfSubDescription: React.FC<SelfSubDescriptionProps> = ({ selfDescriptionDone }) => {
+const SelfSubDescription: React.FC<SelfSubDescriptionProps> = (props) => {
   return (
     <>
-      {selfDescriptionDone && (
-        <Typist className="text-xl text-gray-400 font-zb">
+      {props.selfDescriptionDone && (
+        <Typist
+          className="text-xl text-gray-400 font-zb"
+          onTypingDone={() => props.setSelfSubDescription(true)}
+        >
           Feel free to check out my{" "}
           <Link link="/assets/SavarJain-Resume" color="orange">
             resume
@@ -76,29 +88,36 @@ const SelfSubDescription: React.FC<SelfSubDescriptionProps> = ({ selfDescription
           <Link link="mailto:jainsavar@gmail.com" color="orange">
             jainsavar@gmail.com
           </Link>
+          .
         </Typist>
       )}
     </>
   );
 };
 
-const Socials: React.FC = () => {
+interface SocialsProps {
+  selfSubDescriptionDone: boolean;
+}
+
+const Socials: React.FC<SocialsProps> = (props) => {
   /* TODO: Replace font awesome with a custom svg icons */
   return (
-    <div className="flex justify-between w-32 mt-4">
-      <Link link="https://github.com/SavarJ">
-        <i className="fa fa-github"></i>
-      </Link>
-      <Link link="https://linkedin.com/in/savarjain">
-        <i className="fa fa-linkedin"></i>
-      </Link>
-      <Link link="https://twitter.com/SavarJain">
-        <i className="fa fa-twitter"></i>
-      </Link>
-      <Link link="mailto:jainsavar@gmail.com">
-        <i className="fa fa-envelope"></i>
-      </Link>
-    </div>
+    <Zoom top when={props.selfSubDescriptionDone}>
+      <div className="flex justify-between w-32 mt-4">
+        <Link link="https://github.com/SavarJ">
+          <i className="fa fa-github"></i>
+        </Link>
+        <Link link="https://linkedin.com/in/savarjain">
+          <i className="fa fa-linkedin"></i>
+        </Link>
+        <Link link="https://twitter.com/SavarJain">
+          <i className="fa fa-twitter"></i>
+        </Link>
+        <Link link="mailto:jainsavar@gmail.com">
+          <i className="fa fa-envelope"></i>
+        </Link>
+      </div>
+    </Zoom>
   );
 };
 export default HomeSection;
