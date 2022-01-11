805dbf993cfb0502c00e7d0436b9850342fc5772
Savar Jain: School Laptop <jainsavar@gmail.com>
2022-01-11 12:12:50 -0500

    Minor changes to arrow

--- src/components/sections/Home/MoreArrow.tsx
+++ src/components/sections/Home/MoreArrow.tsx
@@ -8,9 +8,9 @@ const MoreArrow: React.FC<MoreArrowProps> = (props) => {
     <Fade top delay={500} when={props.selfSocialsDone}>
       <div className="arrow-container" id="arrow">
         <a href="#arrow">
-          <div className="arrow arrow-1"></div>
-          <div className="arrow arrow-2"></div>
-          <div className="arrow arrow-3"></div>
+          <div className="arrow arrow-1 w-4 h-4 sm:w-6 sm:h-6 md:w-8 md:h-8"></div>
+          <div className="arrow arrow-2 w-4 h-4 sm:w-6 sm:h-6 md:w-8 md:h-8"></div>
+          <div className="arrow arrow-3 w-4 h-4 sm:w-6 sm:h-6 md:w-8 md:h-8"></div>
         </a>
       </div>
     </Fade>
