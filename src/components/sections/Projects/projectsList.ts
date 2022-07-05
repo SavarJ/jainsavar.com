45e0e428eb762c8e221cdc15ee7fb90d0778a1f3
Savar Jain : Work Mac <jainsavar@gmail.com>
2022-07-05 14:22:09 -0400

    Update to skills store

--- src/components/sections/Projects/projectsList.ts
+++ src/components/sections/Projects/projectsList.ts
@@ -1,9 +1,11 @@
+import { SkillStore, SkillStoreItem } from "../Skills/SkillStore";
+
 interface projectInterface {
   title: string;
   description: string;
   githubLink: string;
   liveLink?: string;
-  languages: string[];
+  skills: SkillStoreItem[];
 }
 
 const projectsList: projectInterface[] = [
@@ -13,7 +15,14 @@ const projectsList: projectInterface[] = [
       "A full stack web application that auto grades programming labs that are assigned to students and displays extensive submission details to the teachers.",
     githubLink: "https://github.com/SavarJ/TheCSAutograder",
     liveLink: "https://thecsautograder.herokuapp.com/",
-    languages: ["Node.js", "Express.js", "MongoDB", "CSS", "Bootstrap", "JavaScript"],
+    skills: [
+      SkillStore.Node,
+      SkillStore.Express,
+      SkillStore.Mongo,
+      SkillStore.EJS,
+      SkillStore.Tailwind,
+      SkillStore.Javascript,
+    ],
   },
   {
     title: "TodoList",
@@ -21,7 +30,7 @@ const projectsList: projectInterface[] = [
       "A fully responsive todolist web application with CRUD (Create, read, update, delete) functionalities. You can also create multiple todolists and add tasks to them.",
     githubLink: "https://github.com/SavarJ/todolist",
     liveLink: "https://savarj-todolist.herokuapp.com/",
-    languages: ["HTML", "CSS", "Node.js", "Express.js", "MongoDB"],
+    skills: [SkillStore.HTMLCSS, SkillStore.Node, SkillStore.Express, SkillStore.EJS, SkillStore.Mongo],
   },
   {
     title: "Simon's Game",
@@ -29,14 +38,14 @@ const projectsList: projectInterface[] = [
       "Implemented my version of the popular Simons game in which users can play using keyboard arrows or by clicking the sqaures.",
     githubLink: "https://github.com/SavarJ/simons-game",
     liveLink: "https://savarj.github.io/simons-game/",
-    languages: ["HTML", "CSS", "JavaScript"],
+    skills: [SkillStore.HTMLCSS, SkillStore.Javascript],
   },
   {
     title: "Emailer",
     description:
       "Programmed a script that will read recipients’ email addresses from a csv file and send emails to them.",
     githubLink: "https://github.com/SavarJ/autoemailer",
-    languages: ["Python"],
+    skills: [SkillStore.Python],
   },
 ];
 
