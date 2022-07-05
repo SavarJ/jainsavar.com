226a05cab41c7c4329d6ec2a3b878c7fb8bcb6ee
Savar Jain : Work Mac <jainsavar@gmail.com>
2022-07-05 13:03:52 -0400

    Add a skills store

---
+++ src/components/sections/Skills/SkillStore.ts
@@ -0,0 +1,102 @@
+const SkillStore = {
+  Language: {
+    style: "bg-blue-800 text-white border-blue-400",
+    items: {
+      Javascript: {
+        value: "JavaScript",
+      },
+      Typescript: {
+        value: "TypeScript",
+      },
+      Java: {
+        value: "Java",
+      },
+      Python: {
+        value: "Python",
+      },
+      HTMLCSS: {
+        value: "HTML/CSS",
+      },
+    },
+  },
+  Framework: {
+    style: "bg-yellow-600 text-white border-yellow-400",
+    items: {
+      React: {
+        value: "React.js",
+      },
+      Node: {
+        value: "Node.js",
+      },
+      Express: {
+        value: "Express.js",
+      },
+      EJS: {
+        value: "EJS",
+      },
+      Flask: {
+        value: "Flask",
+      },
+      Bootstrap: {
+        value: "Bootstrap",
+      },
+      TailwindCSS: {
+        value: "Tailwind CSS",
+      },
+      Next: {
+        value: "Next.js",
+      },
+      Redux: {
+        value: "Redux.js",
+      },
+    },
+  },
+  Database: {
+    style: "bg-green-800 text-white border-green-400",
+    items: {
+      Mongo: {
+        value: "MongoDB",
+      },
+      Datastore: {
+        value: "Google Cloud Datastore",
+      },
+      Postgres: {
+        value: "Postgres SQL",
+      },
+      Dynamo: {
+        value: "Amazon DynamoDB",
+      },
+    },
+  },
+  Tool: {
+    style: "bg-pink-800 text-white border-pink-400",
+    items: {
+      Atlas: {
+        value: "MongoDB Atlas",
+      },
+      Firebase: {
+        value: "Google Firebase",
+      },
+      Heroku: {
+        value: "Heroku",
+      },
+      GitGithub: {
+        value: "Git/GitHub",
+      },
+      Terminal: {
+        value: "Unix/Linux Terminal",
+      },
+      GCP: {
+        value: "Google Cloud Platform (GCP)",
+      },
+      AWS: {
+        value: "Amazon Web Services (AWS)",
+      },
+      Azure: {
+        value: "Microsoft Azure",
+      },
+    },
+  },
+};
+
+export { SkillStore };
--- src/components/sections/Skills/skillsList.ts
+++ src/components/sections/Skills/skillsList.ts
@@ -4,13 +4,3 @@ const databases = ["MongoDB", "Google Cloud Datastore"];
 const tools = ["MongoDB Atlas", "Firebase", "Heroku", "Git", "GitHub", "Unix/Linux Terminal"];
 
 export { languages, frameworks, databases, tools };
-
-const LanguageStyle = "bg-blue-800 text-white border-blue-400";
-const Language = {
-  Javascript: {
-    value: "JavaScript",
-    style: LanguageStyle,
-  },
-};
-
-export { Language };
