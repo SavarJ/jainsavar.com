3a8efcab5d3af1b71fb1562974d42f3458d56f74
Savar Jain - Home MBA <jainsavar@gmail.com>
2024-12-21 17:59:27 -0500

    Datadog

--- src/components/sections/Home/SelfDescription.tsx
+++ src/components/sections/Home/SelfDescription.tsx
@@ -2,32 +2,35 @@ import Link from "../../shared/Link";
 
 const SelfDescription: React.FC = () => {
   return (
-    <>
-      <h1 className="mt-4 text-xl max-w-3xl text-gray-400 font-zb">
-        Prev SWE Intern <span className="font-cabin">@</span>{" "}
-        <Link link="https://github.com/mongodb/mongo/commits/master/?author=savarj" color="mongo">
-          MongoDB
-        </Link>
-        ,{" "}
-        <Link link="https://www.amazon.com/" color="amazon">
-          Amazon
-        </Link>
-        ,{" "}
-        <Link link="https://www.zillow.com/" color="zillow">
-          Zillow
-        </Link>
-        ,{" "}
-        <Link link="https://www.synchrony.com/" color="synchrony">
-          Synchrony
-        </Link>{" "}
-        |{" "}
-        <Link link="https://www.janestreet.com/join-jane-street/programs-and-events/fttp/" color="color-3">
-          Jane Street FTTP '23
-        </Link>{" "}
-        <br />
-        Graduating May 2026!
-      </h1>
-    </>
+    <h1 className="mt-4 text-xl max-w-4xl text-gray-400 font-zb">
+      SWE Intern <span className="font-cabin">@</span>{" "}
+      <Link link="https://www.datadoghq.com/" color="datadog">
+        Datadog 🐶{" "}
+      </Link>
+      | Prev <span className="font-cabin">@</span>{" "}
+      <Link link="https://www.mongodb.com/" color="mongo">
+        MongoDB 🌱
+      </Link>
+      ,{" "}
+      <Link link="https://www.amazon.com/" color="amazon">
+        Amazon 📦
+      </Link>
+      ,{" "}
+      <Link link="https://www.zillow.com/" color="zillow">
+        Zillow 🏠
+      </Link>
+      ,{" "}
+      <Link link="https://www.synchrony.com/" color="synchrony">
+        Synchrony 💳
+      </Link>{" "}
+      <br />
+      <Link link="https://www.janestreet.com/join-jane-street/programs-and-events/fttp/" color="js">
+        Jane Street FTTP '23 📈
+      </Link>{" "}
+      <br />
+      <br />
+      Graduating May 2026!
+    </h1>
   );
 };
 
